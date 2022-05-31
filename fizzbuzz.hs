25c1614c52e2851d677165cb24501182ac1b3e5c
DeCentN2Madness <7983+DeCentN2Madness@users.noreply.github.com>
2022-05-30 20:45:28 -0700

    Add fizzbuzz case version

--- fizzbuzz.hs
+++ fizzbuzz.hs
@@ -5,3 +5,12 @@ fizzbuzz n
     | rem n 3 == 0                 = "fizz"
     | rem n 5 == 0                 = "buzz"
     | otherwise                    = show n
+
+-- fizzbuzz case
+fizzbuzz' :: Int -> String
+fizzbuzz' n =
+    case ( rem n 3 == 0, rem n 5 == 0 ) of
+        (True, True)   -> "fizzbuzz"
+        (True, False)  -> "fizz"
+        (False, True)  -> "buzz"
+        (False, False) -> show n
