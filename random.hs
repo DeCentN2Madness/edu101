26e81bb4c6b866fa804f4d88dcb4dba6dbfecb85
DeCentN2Madness <7983+DeCentN2Madness@users.noreply.github.com>
2022-05-31 07:17:25 -0700

    Define infix function in random.hs

--- random.hs
+++ random.hs
@@ -2,3 +2,8 @@
 five = case 2 + 3 of
     5 -> "yes"
     _ -> "no"
+
+-- define infix function
+(♥) :: String -> String -> Bool
+x ♥ y = (elem x [ "Jeff", "Alice" ] && elem y [ "Sam", "Euterpe" ]) ||
+        (elem x [ "Sam", "Euterpe" ] && elem y [ "Jeff", "Alice" ])
