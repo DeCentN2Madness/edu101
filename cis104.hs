762c6d89622e358b40e03c289bc17ea45bca4a4c
DeCentN2Madness <7983+DeCentN2Madness@users.noreply.github.com>
2022-06-01 10:04:24 -0700

    Add cis104 excercise 1

--- cis104.hs
+++ cis104.hs
@@ -1,3 +1,13 @@
-{- run through of the excercises provided in the pdf file
+{- run-through of the excercises provided in the pdf file
    https://www.cis.upenn.edu/~cis194/spring13/hw/01-intro.pdf
 -}
+
+-- Excercise 1
+
+-- convert positive integers to a list of digits
+toDigits :: Integer -> [Integer]
+toDigits n = [n]
+
+-- convert positive integers to a list of digits reversed
+toDigitsRev :: Integer -> [Integer]
+toDigitsRev n = [n]
