ea36faa6475eb34948fc0ceee03f91c680cf18b2
DeCentN2Madness <7983+DeCentN2Madness@users.noreply.github.com>
2022-06-03 13:39:01 -0700

    add fib'' utilizing golden ratio

--- random.hs
+++ random.hs
@@ -37,16 +37,39 @@ fib' n
     | otherwise = last $ take (fromInteger n + 1) fibs
     where fibs = 0 : scanl (+) 1 fibs
 
+-- fibonacci from golden ratio
+fib'' :: Integer -> Integer
+fib'' n
+    | n < 0     = error "no negatives"
+    | n == 0    = 0
+    | otherwise = last $ take (fromIntegral n) [ round (((1.0 + sqrt 5.0) / 2.0) ** x / sqrt 5.0 ) | x <- [1..] ]
+
 -- Create a function to check if a string is palindrome
 -- "abc" - False
 -- "aba" - True
 palindrome :: [Char] -> Bool
 palindrome s = s == reverse s
 
--- Create a function to count the number of ways you can rearrange the letter of
--- a string to create palindrome.
+-- Create a function to count the number of ways you can rearrange the letters
+-- of a string to create palindromes.
 -- ex - "abc" -> 0
 -- ex - "aabb" -> 2 "abba" and "baab"
 -- Assume the string contains letters "a" .. "z" only.
 numberOfPalindromes :: [Char] -> Integer
-numberOfPalindromes = error "Implement"
+numberOfPalindromes s = error "waiting on implementation"
+
+-- Challenge 1.
+-- Base b, number n -> Write a function which converts number n
+-- to a list of digits in base b. n is a number in base 10.
+base :: Integer -> Integer -> [Integer]
+base n b
+    | b == 8  = [8]
+    | b == 16 = [16]
+    where
+        a = 10
+        b = 11
+        c = 12
+        d = 13
+        e = 14
+        f = 15
+        g = 16
