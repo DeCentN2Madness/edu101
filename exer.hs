cd95fe527ab9c0b037e4573be3d6a5794e11c8da
DeCentN2Madness <7983+DeCentN2Madness@users.noreply.github.com>
2022-06-17 18:58:16 -0700

    setup excercise 1

---
+++ exer.hs
@@ -0,0 +1,9 @@
+-- https://www.youtube.com/playlist?list=PLe7Ei6viL6jGp1Rfu0dil1JH1SHk9bgDV
+
+-- Haskell for Imperative Programmers #5 - List Excercises
+
+{- Excercise #1
+     create a function elem' that returns True if an element is in a given list
+     and returns False otherwise
+-}
+elem' :: (Eq a) => a -> [a] -> Bool
