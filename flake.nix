58de4574ca3f84a5e012f021e54082aea700df8a
DeCentN2Madness <7983+DeCentN2Madness@users.noreply.github.com>
2022-08-17 17:08:31 -0700

    add flake nix and lock files

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "one flake to rule them all";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShell = pkgs.mkShell {
+        nativeBuildInputs = [ pkgs.bashInteractive ];
+        buildInputs = [ ];
+      };
+    });
+}
